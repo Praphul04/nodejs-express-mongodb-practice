4874b27f5befb06754f0207239eb4cf2c74b827a
Praphul04 <praphulthakur6@gmail.com>
2024-08-14 10:53:02 +0530

    search api

--- index.js
+++ index.js
@@ -5,29 +5,17 @@ require("./config");
 const product = require("./product");
 
 const app = express();
+
 app.use(express.json());
-app.post("/create", async (req, res) => {
-  let data = new product(req.body);
-  let result = await data.save();
-  console.log(result);
-  res.send(result);
-});
 
-app.get("/list", async (req, res) => {
-  let data = await product.find();
-  res.send(data);
-});
-
-app.delete("/delete/:_id", async (req, res) => {
-  console.log(req.params);
-  let data = await product.deleteOne(req.params);
-  res.send(data);
-});
-
-app.put("/update/:_id", async (req, res) => {
-  console.log(req.params);
-  let data = await product.updateOne(req.params, {
-    $set: req.body,
+app.get("/search/:key", async (req, res) => {
+  console.log(req.params.key);
+  let data = await product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } }
+    ],
   });
   res.send(data);
 });
