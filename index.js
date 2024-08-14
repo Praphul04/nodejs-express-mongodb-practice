f458e6219a1706ef387c504c735e3009a9c2f740
Praphul04 <praphulthakur6@gmail.com>
2024-08-14 09:30:20 +0530

    mongoose

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-const dbConnect = require('./mongodb');
-// dbConnect().then((resp) => {
-//   resp
-//     .find({})
-//     .toArray()
-//     .then((data) => {
-//       console.warn(data);
-//     });
-// });
+const mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/e-comm");
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  brand: String,
+  price: Number,
+  category: String,
+});
 
-const main = async () => {
-    let data = await dbConnect();
-    data = await data.find().toArray();
-    console.warn(data);
+const saveInDB = async () => {
+  const ProductsModel = mongoose.model("products", ProductSchema);
+  let data = new ProductsModel({
+    name: "m30",
+    brand: "realme",
+    price: 2000,
+    category: "mobiles",
+  });
+  let result = await data.save();
+  console.log(result);
+};
 
+const updateInDB = async () => {
+  const ProductsModel = mongoose.model("products", ProductSchema);
+  let data = await ProductsModel.updateOne({
+    name: "m30"},
+    {$set: {name: "redmi 9", price: 1150 }
+ } );
+  console.log(data);
+};
+
+const deleteInDb = async () => {
+    const ProductsModel = mongoose.model("products", ProductSchema);
+    let data = await ProductsModel.deleteOne({name:"m20"});
+    console.log(data);
 }
-main();
+
+
+const findInDB = async () => {
+    const ProductsModel = mongoose.model("products", ProductSchema);
+    let data = await ProductsModel.find();
+    console.log(data);
+}
+findInDB();
